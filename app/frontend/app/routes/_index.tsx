0417bd0624a25cd8f13e57ba648950811925ca05
Jake Laderman <jsladerman@gmail.com>
2024-10-03 22:10:33 -0400

    load basic data

--- app/frontend/app/routes/_index.tsx
+++ app/frontend/app/routes/_index.tsx
@@ -1,5 +1,8 @@
 import type { MetaFunction } from "@vercel/remix";
 
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
 export const meta: MetaFunction = () => {
   return [
     { title: "Sport Models" },
@@ -7,35 +10,68 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async () => {
+  const response = await fetch(
+    "https://j1neh7th0m.execute-api.us-east-1.amazonaws.com/nfl-picks",
+    {
+      headers: {
+        Authorization: process.env.AUTHORIZATION_TOKEN ?? "",
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Response("Failed to fetch data", { status: response.status });
+  }
+
+  const data = await response.json();
+  return json(data);
+};
+
 export default function Index() {
+  const data = useLoaderData();
+
+  if (!Array.isArray(data) || data.length === 0) {
+    return <div>No data available</div>;
+  }
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th key={column}>{column}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {columns.map((column) => (
+              <td key={column}>{row[column]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
+
+const columns = [
+  "season",
+  "week",
+  "home_team",
+  "away_team",
+  "home_score_pred",
+  "away_score_pred",
+  "spread_pred",
+  "spread_line",
+  "spread_play",
+  "spread_win_prob",
+  "spread_lock",
+  "total_pred",
+  "total_line",
+  "total_play",
+  "total_win_prob",
+  "total_lock",
+];
